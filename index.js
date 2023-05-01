a4cb965347418bd220a35bc8cb895c4962ba8b8f
cesargusa <cesargusa22@gmail.com>
2023-05-01 17:58:35 +0200

    Cambiado para pro

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const userRoute = require('.//routes/userRoutes')
 const app = express()
 const cors = require('cors');
 
-
 app.use(express.json());
 app.use(cors())
 //Middelware, path inicial
 app.use('/api/Users', userRoute);
 
-app.listen(3000,() =>{
-    console.log("Funciona en el puerto 3000")
-})
+const port = process.env.PORT || 3000; // Selecciona el puerto disponible o el 3000 por defecto
+
+app.listen(port, () => {
+  console.log(`La aplicación está corriendo en el puerto ${port}`);
+});
